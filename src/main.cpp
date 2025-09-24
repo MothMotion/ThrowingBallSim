7070487d93f7a65fdd880f7b6ee0302b83e1229d
Kali <kalimdenviroment@gmail.com>
2025-09-24 09:21:50 +0500

    added finding optimal time step for req accuracy

--- src/main.cpp
+++ src/main.cpp
@@ -20,55 +20,70 @@ int main (int argc, char *argv[]) {
     return -1;
   }
 
+  // Ball radius
   const double radius = atof(argv[3]),
+  // Ring, left point
                left_x = atof(argv[6]),
+  // Ring, right point
                right_x = atof(argv[7]),
+  // Ring elevation, y
                elevation = atof(argv[8]),
-               delta = atof(argv[9]),
-               eps = atof(argv[10]);
-  const vec2 grav(atof(argv[11]), atof(argv[12]));
-
-  Step* steps[2]{new Step(atof(argv[1]), atof(argv[2]), atof(argv[4]), atof(argv[5])), new Step()};
+  // Epsilon, accuracy
+               eps = atof(argv[9]);
+  // Time step
+  double       delta = atof(argv[9]);
+  const vec2 grav(atof(argv[11]), atof(argv[12])); 
 
   const int screenWidth = 800,
             screenHeight = 450;
 
   InitWindow(screenWidth, screenHeight, "MatAn Simulaiton");
 
-  SetTargetFPS(60);
+  SetTargetFPS(240);
 
-  vec2 max_view(radius, radius);
-  double view_scale = 0;
+  Step* steps[2]{new Step(), new Step};
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-  while(!registerHit(*steps[0], radius, eps, left_x, right_x, elevation) &&
-        steps[0]->pos.y >= -eps) {
-    nextStep(delta, grav, *steps[0], *steps[1]);
-    std::swap(steps[0], steps[1]);
-    std::cout << "\nCycle\n";
+  // Run simulation
+  do { 
+    *steps[0] = Step(atof(argv[1]), atof(argv[2]), atof(argv[4]), atof(argv[5]));
+    *steps[1] = Step();
 
-    BeginDrawing();
-      ClearBackground(RAYWHITE);
-      max_view.x = std::max(max_view.x * SFALLOFF, std::max(steps[0]->pos.x + radius/2, right_x + radius/2));
-      max_view.y = std::max(max_view.y * SFALLOFF, std::max(steps[0]->pos.y + radius/2, elevation));
-      view_scale = std::min(screenWidth / max_view.x, screenHeight / max_view.y);
-      DrawText(TextFormat("Scale:%.2f;\nPos(%.2f;%.2f);\n", view_scale, steps[0]->pos.x, steps[0]->pos.y), 10, 10, 16, BLACK);
+    vec2 max_view(radius, radius);
+    double view_scale = 0;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    while(!registerHit(*steps[0], radius, eps, left_x, right_x, elevation) &&
+          steps[0]->pos.y >= -eps) {
+      nextStep(delta, grav, *steps[0], *steps[1]);
+      std::swap(steps[0], steps[1]); 
+
+      // Visualisation, real time
+      BeginDrawing();
+        ClearBackground(RAYWHITE);
+        max_view.x = std::max(max_view.x * SFALLOFF, std::max(steps[0]->pos.x + radius/2, right_x + radius/2));
+        max_view.y = std::max(max_view.y * SFALLOFF, std::max(steps[0]->pos.y + radius/2, elevation));
+        view_scale = std::min(screenWidth / max_view.x, screenHeight / max_view.y); 
  
-      drawStep(screenWidth, screenHeight, steps[0]->pos.x * view_scale, steps[0]->pos.y * view_scale, radius * view_scale,
-               left_x * view_scale, right_x * view_scale, elevation, 1 * view_scale, 12 * view_scale, GREEN, RED, BLUE);
-    EndDrawing();
-  }
+        drawStep(screenWidth, screenHeight, steps[0]->pos.x * view_scale, steps[0]->pos.y * view_scale, radius * view_scale,
+                 left_x * view_scale, right_x * view_scale, elevation, 1 * view_scale, 12 * view_scale, GREEN, RED, BLUE);
+        DrawText(TextFormat("Scale:%.2f;\nPos(%.2f;%.2f);\nTime step:%.4f\nFPS:%d", view_scale, steps[0]->pos.x, steps[0]->pos.y, delta,
+                            GetFPS()), 10, 10, 16, BLACK);
+      EndDrawing();
+    }
 
-  std::cout << "\n\n??? > " << steps[0]->pos.y << "\n";
-  if(steps[0]->pos.y < -eps) {
-    if(left_x + eps <= steps[0]->pos.x && steps[0]->pos.x <= right_x - eps)
-      std::cout << "DUNK!\n";
-    else
-      std::cout << "no dunk :(\n";
-  } else
-    std::cout << "Hit ring\n";
-  std::cout << "Throw accuracy: " << std::abs((right_x + left_x)/2 - steps[0]->pos.x);
-  std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    // Result of throw 
+    if(steps[0]->pos.y < -eps) {
+      if(left_x + eps <= steps[0]->pos.x && steps[0]->pos.x <= right_x - eps)
+        std::cout << "DUNK!\n";
+      else
+        std::cout << "no dunk :(\n";
+    } else
+      std::cout << "Hit ring\n";
+    std::cout << "Throw accuracy: " << std::abs((right_x + left_x)/2 - steps[0]->pos.x) << "\n"
+              << "Current deviation: " << steps[1]->pos.y - steps[0]->pos.y << "\nRequered: " << eps << "\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    delta *= 0.5; 
+  } while(steps[1]->pos.y - steps[0]->pos.y > eps);
 
   CloseWindow();
 
